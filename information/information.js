fa5dedd62c63b4de70e8b6ee944187ef8895ec34
sabin5105 <sabin5105@gmail.com>
2022-11-26 16:59:09 +0900

    add kakao share

--- information/information.js
+++ information/information.js
@@ -5,8 +5,9 @@ var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 var time = document.querySelector('#time')
+var kakaoShare = document.querySelector('#kakaotalk-sharing-btn')
 
-apik = "3045dd712ffe6e702e3245525ac7fa38"
+apik = "" // api key
 
 function convertion(val){
     return (val - 273).toFixed(2)
@@ -37,4 +38,39 @@ btn.addEventListener('click', function(){
 
     })
     .catch(err => alert('You entered Wrong city name'))
+})
+
+kakaoShare.addEventListener('click', function(){
+    // TODO:전부다 병합하면 주소 변경해서 추가할 예정
+    Kakao.Share.createDefaultButton({
+    container: '#kakaotalk-sharing-btn',
+    objectType: 'feed',
+    content: {
+        title: '날씨 정보',
+        description: '날씨 정보를 확인하세요',
+        imageUrl:
+        'https://cdn-icons-png.flaticon.com/512/1669/1669524.png',
+        link: {
+        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+        mobileWebUrl: 'http://localhost:3000',
+        webUrl: 'http://localhost:3000',
+        },
+    },
+    buttons: [
+        {
+        title: '웹으로 보기',
+        link: {
+            mobileWebUrl: 'http://localhost:3000',
+            webUrl: 'http://localhost:3000',
+        },
+        },
+        {
+        title: '앱으로 보기',
+        link: {
+            mobileWebUrl: 'http://localhost:3000',
+            webUrl: 'http://localhost:3000',
+        },
+        },
+    ],
+    });
 })
