65e1a06911279d168e7bb55897126b037386b1a7
sabin5105 <sabin5105@gmail.com>
2022-11-26 15:53:10 +0900

    add signup

--- login/login.js
+++ login/login.js
@@ -70,4 +70,60 @@ app.get('/home', function(request, response) {
 	response.end();
 });
 
+// http://localhost:3000/newaccount
+app.get('/newaccount', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('You have a problems with sign up! Maybe you are already registered with the same name?');
+	}
+	response.end();
+});
+
+// http://localhost:3000/signup
+app.get('/signup', function(request, response) {
+	// Render signup template
+	response.sendFile(path.join(__dirname + '/signup.html'));
+});
+
+// http://localhost:3000/signauth
+app.post('/signauth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		connection.query('SELECT * FROM userTable WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is same user, output the error
+			if (results.length > 0) {
+				response.send('You are already registered!');
+			} else {
+				// Execute SQL query that'll select the account from the database based on the specified username and password
+				connection.query('INSERT INTO userTable (username, password) VALUES (?, ?)', [username, password], function(error, results, fields) {
+					// If there is an issue with the query, output the error
+					if (error) throw error;
+					// If the account exists
+					if (results.length > 0) {
+						// Authenticate the user
+						request.session.loggedin = true;
+						request.session.username = username;
+						// Redirect to home page
+						response.redirect('/newaccount');
+					} else {
+						response.send('Incorrect Username and/or Password!');
+					}
+					response.end();
+				});
+			}
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
 app.listen(3000);
