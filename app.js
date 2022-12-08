47211050a1ba26bf3de00c287003e19dbf4229e0
wldnjs7064@naver.com <wldnjs7064@naver.com>
2022-12-09 00:05:14 +0900

    news console

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const fileStore = require('session-file-store')(session);
 const path = require('path');
+const request = require("request");
+const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
 
 // mysql connection
 const connection = mysql.createConnection({
@@ -140,6 +143,47 @@ app.post('/signauth', function(request, response) {
 		response.end();
 	}
 });
+
+app.get("/naver_weather", (req, res) => {
+  
+	request(
+	  {
+		url: "https://weather.naver.com/news/",
+		method: "GET",
+		encoding: null,
+	  },
+	  (error, response, body) => {
+		if (error) {
+		  console.error(error);
+		  return;
+		}
+		if (response.statusCode === 200) {
+		  console.log("response ok");
+		  const bodyDecoded = iconv.decode(body, "utf-8");
+		  const $ = cheerio.load(bodyDecoded);
+  
+		  img_class = "thumb";
+		  title_class = "tit_news";
+		  document_class = "dsc_news";
+  
+		  // get top 10 news
+		  news = [];
+		  for (let i = 1; i < 11; i++) {
+			img = $("." + img_class).eq(i).attr("src");
+			title = $("." + title_class).eq(i).text();
+			document = $("." + document_class).eq(i).text();
+			news.push({
+			  img: img,
+			  title: title,
+			  document: document,
+			});
+		  }
+		  console.log(news);
+		  res.send(news);
+		}
+	  }
+	);
+  });
 
 
 app.listen(3000);
--- news/naver_weather.js
+++ news/naver_weather.js
@@ -1,42 +1,12 @@
-const request = require("request");
-const cheerio = require("cheerio");
-const iconv = require("iconv-lite");
-const getNews = () => {
-    request(
-    {
-        url: "https://weather.naver.com/news/",
-        method: "GET",
-        encoding: null,
-    },
-    (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    if (response.statusCode === 200) {
-        console.log("response ok");
-        const bodyDecoded = iconv.decode(body, "utf-8");
-        const $ = cheerio.load(bodyDecoded);
-        
-        img_class = 'thumb'
-        title_class = 'tit_news'
-        document_class = 'dsc_news'
-
-        // get top 5 news
-        news = []
-        for (let i = 1; i < 6; i++) {
-            img = $('.' + img_class).eq(i).attr('src')
-            title = $('.' + title_class).eq(i).text()
-            document = $('.' + document_class).eq(i).text()
-            news.push({
-                img: img,
-                title: title,
-                document: document
-            })
-        }
-        console.log(news)
-    }
-  });
-};
-
-setInterval(getNews, 1000 * 60 * 10); // 10분마다 실행
+function getData() {
+    fetch("/naver_weather")
+      .then((response) => response.json())
+      .then((news) => {
+        console.log(news);
+      });
+  }
+  
+  window.onload = function () {
+    getData();
+    setInterval(getData, 1000 * 60 * 10); // 10분마다 실행
+  };
